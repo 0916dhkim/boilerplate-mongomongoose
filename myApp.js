5fb2d56a4181c43c7877b7c52ff810b9c97700db
Donghyeon Kim <0916dhkim@gmail.com>
2020-05-15 17:31:28 +0900

    Implement Find Function

--- myApp.js
+++ myApp.js
@@ -133,9 +133,10 @@ var createManyPeople = function(arrayOfPeople, done) {
 // Use the function argument `personName` as search key.
 
 var findPeopleByName = function(personName, done) {
-  
-  done(null/*, data*/);
-
+  Person.find(
+    { name: personName },
+    done
+  );
 };
 
 /** 6) Use `Model.findOne()` */
